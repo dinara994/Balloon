f119cb3f2281a00d852420ce2397e7c0fce103a7
dinara994 <di_94@mail.ru>
2021-08-05 19:26:26 +0600

    initial

---
+++ js/script.js
@@ -0,0 +1,51 @@
+const balloon = document.querySelector('.balloon')
+const box = document.querySelector('.box')
+const box1 = document.querySelectorAll('.box-1')
+const balllonImg = document.querySelectorAll('.balloon-img')
+const bx = document.querySelector('.bx')
+
+let idx = 0
+
+balloon.addEventListener('click', (ev) => {
+    if (ev.target.classList[0] === 'balloon-img') {
+        box1[idx].src = ev.target.src
+        idx++
+    }
+})
+bx.addEventListener('click', () => {
+    box1.forEach(el => el.src = "./img/standard_white.gif")
+    idx = 0
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
